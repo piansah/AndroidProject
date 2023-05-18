796a8e89e446784f02c8721c363dd0ab48444724
Pian <alifalpian157@gmail.com>
2023-05-18 10:14:08 +0700

    code

---
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.sharedpreferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sharedpreferences.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var sharedPrefs: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPrefs = getSharedPreferences("DataUser", Context.MODE_PRIVATE)
+
+        val userId = sharedPrefs.getInt("user_id", -1)
+        val userName = sharedPrefs.getString("user_name", "")
+
+        binding.textHello.text = "Hello$userName"
+
+        // Handle your other views and logic here
+    }
+}
